9874e1c1b827bf8cd6b4878089a73f9b5e23fc92
Plinio Gonçalves <plinio.goncalves@verdecard.com.br>
2025-06-02 04:51:54 -0300

    fix: ajuste cors

--- services/auth-service/src/server.js
+++ services/auth-service/src/server.js
@@ -1,40 +1,17 @@
-// auth-service/src/server.js
 const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
 const express = require('express');
-const cors = require('cors'); // Certifique-se que está importado
+const cors = require('cors');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
-// const { protectAuth } = require('./middleware/authMiddleware'); // Se você moveu para cá
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 connectDB();
 
-const whitelist = ['https://bairro-digital.vercel.app'];
-if (process.env.NODE_ENV !== 'production') {
-    whitelist.push('http://localhost:5500');
-    whitelist.push('http://127.0.0.1:5500');
-}
-
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'], 
-  credentials: true
-};
-
-app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
-
+app.use(cors());
 
 app.use(express.json());
 
