16698e1e9f929265c8b6e76ccdd396e3ab7ca3f9
Plinio Gonçalves <plinio.goncalves@verdecard.com.br>
2025-06-02 09:02:11 -0300

    fix: ajuste no cors do occurrences services do vercel e render

--- services/occurrences-service/src/server.js
+++ services/occurrences-service/src/server.js
@@ -2,7 +2,7 @@ const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
 const express = require('express');
-const cors = require('cors'); 
+const cors = require('cors');
 const connectDB = require('./config/db');
 const occurrenceRoutes = require('./routes/occurrenceRoutes');
 
@@ -11,27 +11,7 @@ const PORT = process.env.PORT || 3002;
 
 connectDB();
 
-const whitelist = ['https://bairro-digital.vercel.app'];
- if (process.env.NODE_ENV !== 'production') {
-     whitelist.push('http://localhost:5500');
-     whitelist.push('http://127.0.0.1:5500');
-}
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
-};
-
-app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
-
+app.use(cors());
 
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
